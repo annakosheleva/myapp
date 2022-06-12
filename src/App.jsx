b750910665ac2c98eb4e0dd8cf4d404faa2209b9
annakosheleva <71738312+annakosheleva@users.noreply.github.com>
2022-06-12 15:47:36 +0300

    hw2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import "./App.css";
-import Message from "./Message";
+import React, {useState} from "react";
+import "./styles/App.css";
+import MessageList from './components/MessageList';
+import MessageForm from "./components/MessageForm";
+
+
 
 function App() {
-	const name = "Anna";
-	const message = "My First React App";
+	const [messages, setMessages] = useState([
+		{ id: 1, author: 'Ann', text: 'Hello)' },
+		{ id: 2, author: 'Mark', text: 'Hi! How are you?' },
+		{ id: 3, author: 'Lisa', text: 'Hi, all..' },
+	])
 
+	const createMessage = (newMessage) => {
+		setMessages([...messages, newMessage])
+	}
+
+	const removeMessage = (message) => {
+		setMessages(messages.filter(m => m.id !== message.id))
+	}
+	
 	return (
 		<div className="App">
-			<header className="App-header">
-				<Message props={message} />
-				<h1>Hello, {name}</h1>
-			</header>
+			<MessageForm create={createMessage} />
+			{messages.length !== 0
+				?
+				<MessageList remove={removeMessage} messages={messages} title='List of messages' />
+				:
+				<h1 style={{ textAlign: 'center' }}>No messages</h1>
+			}
+			
 		</div>
 	);
 }
